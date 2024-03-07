29eaa4ef38c48b450f4bdb12c9731516bddc43bb
ShinChven <shinchven@gmail.com>
2024-03-07 15:31:49 +0800

    feat(:wrench: context-menu): Implement sharing to multiple platforms
    - Add "Share This Page" option to the context menu
    - Create suboptions for sharing on Twitter, Facebook, and Reddit
    - Add an option to copy the sharing text to the clipboard

--- src/background.js
+++ src/background.js
@@ -1,38 +1,102 @@
 chrome.runtime.onInstalled.addListener(function() {
+  // Create a parent item
   chrome.contextMenus.create({
-    id: "tweetPage",
-    title: "Tweet This Page",
+    id: "sharePage",
+    title: "Share This Page",
+    contexts: ["page"]
+  });
+
+  // Twitter
+  chrome.contextMenus.create({
+    id: "shareTwitter",
+    parentId: "sharePage",
+    title: "Share on Twitter",
+    contexts: ["page"]
+  });
+
+  // Facebook
+  chrome.contextMenus.create({
+    id: "shareFacebook",
+    parentId: "sharePage",
+    title: "Share on Facebook",
+    contexts: ["page"]
+  });
+
+  // Reddit
+  chrome.contextMenus.create({
+    id: "shareReddit",
+    parentId: "sharePage",
+    title: "Share on Reddit",
+    contexts: ["page"]
+  });
+
+  // Copy text
+  chrome.contextMenus.create({
+    id: "copyText",
+    parentId: "sharePage",
+    title: "Copy Sharing Text",
     contexts: ["page"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "tweetPage") {
+  if (info.menuItemId.startsWith("share")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: getPageDetails
+      function: getPageDetails,
+      args: [info.menuItemId]
     });
   }
 });
 
-function getPageDetails() {
-  const pageTitle = document.title; // Get the page title
-  let pageDescription = ''; // Variable to hold the description
-
-  // Try to extract the content of the meta description tag
-  const metaDescription = document.querySelector("meta[name='og:description']");
-  if (metaDescription) {
-    pageDescription = metaDescription.content;
+function getPageDetails(menuItemId) {
+  const pageTitle = document.title;
+  
+  // Function to retrieve content from meta tags by property name
+  function getMetaContentByName(name) {
+    const tag = document.querySelector(`meta[property='${name}'], meta[name='${name}']`);
+    return tag ? tag.content : null;
   }
 
-  // Truncate the description to 100 characters for example
-  if (pageDescription.length > 100) {
-    pageDescription = pageDescription.substring(0, 100) + '...';
+  // Try to get the description from various meta tags
+  let pageDescription = getMetaContentByName('og:description') ||
+                        getMetaContentByName('description') ||
+                        getMetaContentByName('twitter:description');
+
+  // Create an array to hold the parts of the text
+  const textParts = [pageTitle];
+
+  // If description exists, add it to the array
+  if (pageDescription) {
+    textParts.push(pageDescription);
   }
 
-  // Construct the Twitter URL with both page title and description
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(pageTitle)} - ${encodeURIComponent(pageDescription)}&url=${encodeURIComponent(document.location.href)}`;
+  // Join the parts with a separator, but only if the description is available
+  const text = textParts.join(' - ');
 
-  // Open Twitter URL in a new tab
-  window.open(twitterUrl, '_blank');
+  const pageUrl = document.location.href;
+  let shareUrl = '';
+
+  switch (menuItemId) {
+    case 'shareTwitter':
+      shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(pageUrl)}`;
+      break;
+    case 'shareFacebook':
+      shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pageUrl)}`;
+      break;
+    case 'shareReddit':
+      shareUrl = `https://www.reddit.com/submit?url=${encodeURIComponent(pageUrl)}&title=${encodeURIComponent(text)}`;
+      break;
+    case 'copyText':
+      navigator.clipboard.writeText(`${text} ${pageUrl}`).then(() => {
+        alert('Sharing text copied to clipboard!');
+      }, (err) => {
+        console.error('Could not copy text: ', err);
+      });
+      return; // Exit the function after copying to clipboard
+  }
+
+  if (shareUrl) {
+    window.open(shareUrl, '_blank');
+  }
 }
